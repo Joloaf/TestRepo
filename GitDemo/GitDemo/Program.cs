1de1d1b7a9d5b22a956d18c55e9577ba36645f1e
Joachim Carlsson <carlsson.joachim@gmail.com>
2024-10-23 13:07:37 +0200

    Added movie question line

--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Who's your daddy and what does he do?");
             KindergartenCop.WriteText("I'm detective John Kimble!");
+            Console.WriteLine("What movie is this from?");
             KindergartenCop.MyLoop();
         }
 
