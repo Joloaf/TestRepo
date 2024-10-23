6158ed2bbaa3b2d04d79de61909b7f10f3cee128
Joachim Carlsson <carlsson.joachim@gmail.com>
2024-10-23 11:01:10 +0200

    Created usless class file

---
+++ GitDemo/GitDemo/KindergartenCop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class KindergartenCop
+    {
+        public static void WriteText(string text)
+        {
+            Console.WriteLine("I'm detective John Kimble!");
+        }
+    }
+}
