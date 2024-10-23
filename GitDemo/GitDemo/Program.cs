1c6b6195eadce22095034a61641a34fafff708a4
Joachim Carlsson <carlsson.joachim@gmail.com>
2024-10-23 10:56:56 +0200

    Minor changes to empty project

--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Who's your daddy and what does he do?");
         }
     }
 }
